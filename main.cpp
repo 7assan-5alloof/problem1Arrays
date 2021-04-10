3cd93ec5c95b83299a48b7b66867f901335558a2
7assan-5alloof <hassankhalloof20@gmail.com>
2021-04-10 20:54:14 +0400

    Write program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int arr[10] = {12, 45, 67, 33, 22, -34, 56, 78, 44, 99}; // Given array for problem
+    int position; cout << "Enter a position: "; cin >> position; // Ask user for input
+    int base = arr[position]; // Fetch data from specified position
+
+    // Find numbers less on the left
+    int smaller = 0;
+    for (int i = 0; i < position; i++) if (arr[i] < base) smaller++;
+    cout << "There ";
+    if (smaller > 3) cout << "are";
+    else cout << "is";
+    cout << " " << smaller << " element";
+    if (smaller > 2) cout << "s";
+    cout << " smaller than " << base << " on its left and ";
+
+    // Find numbers greater on the right
+    int bigger = 0;
+    for (int i = 9; i > position; i--) if (arr[i] > base) bigger++;
+    cout << bigger << " element";
+    if (bigger > 2) cout << "s";
+    cout << " greater than it on its right.";
+
     return 0;
 }
